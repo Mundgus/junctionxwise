2b43914a94dad8cd22d96d6f37997ebca56abe9a
erdar2 <aron98@gmail.com>
2022-10-23 00:48:27 +0200

    matter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {Engine} from "react-matter-js";
 
 function App() {
   return (
     <div style={{height: 2436, width: 1125, backgroundImage: "url(/images/iphone.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
+      <Engine>
 
+      </Engine>
     </div>
   );
 }
