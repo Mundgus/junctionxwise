da4b0fac06e959903f1f525e1b26494f69b2a176
erdar2 <aron98@gmail.com>
2022-10-23 00:09:40 +0200

    background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: 2436, width: 1125, backgroundImage: "url(/images/iphone.png)", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
+
     </div>
   );
 }
